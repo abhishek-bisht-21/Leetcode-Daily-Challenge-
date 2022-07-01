ea7076d6d9bc07f17d1fead99cfa7713aebfbd3b
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-07-01 23:12:32 +0530

    Leetcode daily June 1 2022

--- July 2022/1.July.cpp
+++ July 2022/1.July.cpp
@@ -1,5 +1,6 @@
 // 1710. Maximum Units on a Truck
 
+// Approach 1 : SORTING WITH COMPARATOR
 class Solution {
 public:
     static bool myfunction(vector<int>& a, vector<int>& b){
@@ -19,3 +20,39 @@ public:
         return ans;
     }
 }
+
+// Approach 2: PRIORITY QUEUE
+
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        
+        priority_queue<pair<int,int>> pq;
+        
+        for(auto &box: boxTypes){
+            pq.push({box[1], box[0]});
+        }
+        
+        int units = 0;
+        int count = 0;
+        
+        while(!pq.empty() && count < truckSize){
+            
+            if(count + pq.top().second < truckSize){
+                
+                count += pq.top().second;
+                units += pq.top().second*pq.top().first;
+                pq.pop();
+            }else{
+                
+                
+                int qty = truckSize - count;
+                count += qty;
+                units += qty * pq.top().first;
+                pq.pop();
+            }
+        }
+        
+        return units;
+    }
+};
