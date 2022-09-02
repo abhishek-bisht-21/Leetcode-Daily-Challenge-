6eae0e1bafccf7a2191d3acd96e179be0d6c529c
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-09-02 22:24:54 +0530

    637. Average of Levels in Binary Tree

---
+++ Sept 2022/2.Sept_2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        
+        vector<double> res;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            long temp = 0;
+            int s = q.size();
+            for(int i=0;i<s;i++){
+                TreeNode* t = q.front();
+                q.pop();
+                temp+=t->val;
+                
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+            
+            res.push_back((double)temp/s);
+        }
+        
+        return res;
+    }
+};
